34818febaa7f0151b874621350db9604309af9e1
Destructor_Ben <ben.honeyfield@gmail.com>
2024-11-03 14:15:45 +1300

    Delete shitty footer code

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,33 +1,4 @@
-<script>
-  import { browser } from '$app/environment';
-
-  function goToTop()
-  {
-    window.scroll(0, 0);
-    history.pushState({}, "", window.location.pathname + window.location.search);
-  }
-
-  // TODO: this is a bit dodgy while loading, maybe just make an opt out for certain pages?
-  // TODO: I don't even think this works properly
-  function needsGoToTopButton()
-  {
-    if (!browser)
-      return false;
-
-    const body = document.body;
-    const bodyHeight = body.getBoundingClientRect().height;
-    const viewportHeight = window.innerHeight;
-    return bodyHeight > viewportHeight;
-  }
-</script>
-
 <footer>
-  {#if needsGoToTopButton()}
-    <span>You've reached the bottom</span>
-    <button on:click={goToTop}>Go back to the top</button>
-    <hr />
-  {/if}
-
   <div>
     <div>Copyright © 2024 Destructor_Ben. MIT License. All rights reserved.</div>
     <div>Made with love & SvelteKit in Aotearoa New Zealand</div>
